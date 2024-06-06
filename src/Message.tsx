490ee093c46a7d0793db89f33d0dd133c7749a9c
ChangbeomAhn <changbeomahn@ChangbeomAhnui-Macmini.local>
2024-06-07 04:14:36 +0900

    add

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,13 +1,9 @@
 import { useState, useEffect } from 'react'
 import { auth, onSocialClick, dbservice, storage } from './serverbase'
 import { collection, query, where, orderBy, addDoc, getDocs, doc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore';
-// import supporting from './supporting';
 import Btn from './Btn';
 import Avatars from './Avatars';
 import FastRewindIcon from '@mui/icons-material/FastRewind';
-// import confirm from './confirm';
-// import confirming from './confirming';
-// import Dialogs from './Dialogs';
 import { BrowserRouter, Routes, Route, useNavigate, Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -16,12 +12,6 @@ import CardContent from '@mui/material/CardContent';
 function Message({ msgObj, isOwner, userObj, isLoggedIn, counter, setCounter, setValue }) {
   const [num, setNum] = useState(null)
   const [points, setPoints] = useState(null)
-  // const [move, setMove] = useState(false)
-  
-  // const onDeleteClick = () => {
-  //   const data = doc(dbservice, `num/${msgObj.id}`)
-  //   deleteDoc(data)
-  // }
 
   useEffect(() => {
     onSnapshot(query(doc(dbservice, `members/${msgObj.creatorId}`)), (snapshot) => {
@@ -87,68 +77,15 @@ function Message({ msgObj, isOwner, userObj, isLoggedIn, counter, setCounter, se
         {msgObj.text.choose == 2 &&
           <div className='d-flex justify-content-center'>빌려주기</div>
         }
-        {/* <div className='d-flex justify-content-center'>요청 유저 이름: {msgObj.displayName}</div>
-        <div className='d-flex justify-content-center'>포인트: {msgObj.point}</div> */}
         <div className='d-flex justify-content-center'>
           <Avatars altName={msgObj.displayName} />
           <FastRewindIcon />
           <Avatars />
         </div>
         <div className='d-flex justify-content-center'>{msgObj.text.counting} {msgObj.text.counter}</div>
-        {/* <div className='d-flex justify-content-center'>{msgObj.text.counter}</div> */}
         <div className='d-flex justify-content-center'>{msgObj.text.clock.year}.{msgObj.text.clock.month}.{msgObj.text.clock.day} {msgObj.text.clock.hour}:{msgObj.text.clock.minute}</div>
         <div className='d-flex justify-content-center'>~</div>
         <div className='d-flex justify-content-center'>{msgObj.text.clock.year}.{msgObj.text.clock.month}.{msgObj.text.clock.day} {msgObj.text.clocker.hour}:{msgObj.text.clocker.minute}</div>
-        {/* <div className='d-flex justify-content-center'>승낙 유저 이름: {msgObj.connectedName}</div>
-        <div className='d-flex justify-content-center'>진행 단계: {msgObj.round}</div> */}
-        {/* <Btn msgObj={msgObj} isOwner={isOwner} userObj={userObj} num={num} value={value} /> */}
-        {/* {isOwner &&
-          <div className='d-flex justify-content-center'>
-            {msgObj.round === 1 && <button className='d-flex justify-content-center btn btn-outline-primary' onClick={onDeleteClick}>지우기</button>}
-            {msgObj.round === 2 &&
-              <button className='d-flex justify-content-center btn btn-outline-primary' onClick={() => confirm({userObj, msgObj})}>승낙 메시지 확인</button>
-            }
-            {msgObj.round === 3 &&
-              <div className='d-flex justify-content-center'>
-                {msgObj.text.choose == 1 && <button className='d-flex justify-content-center btn btn-outline-primary' onClick={() => confirming({userObj, msgObj})}>반납하기</button>}
-                {msgObj.text.choose == 2 && <button className='d-flex justify-content-center btn btn-outline-primary'>{msgObj.connectedName} 님이 빌리는 중</button>}
-              </div>
-            }
-            {msgObj.round === 4 &&
-              <div className='d-flex justify-content-center'>
-                {msgObj.text.choose == 1 && <button className='d-flex justify-content-center btn btn-outline-primary'>주인에게 확인 중</button>}
-                {msgObj.text.choose == 2 && <button className='d-flex justify-content-center btn btn-outline-primary' onClick={onClick}>반납 완료 확인</button>}
-              </div>
-            }
-          </div>
-        }
-        {!isOwner &&
-          <div className='d-flex justify-content-center'>
-            {msgObj.round === 1 &&
-              <div className='d-flex justify-content-center'>
-                <button className='d-flex justify-content-center btn btn-outline-primary' onClick={() => support(userObj, msgObj, isLoggedIn)}>승낙하기</button>
-                <Dialogs move={move} handleClose={handleClose}/>
-              </div>
-            }
-            {msgObj.round === 2 &&
-              <div className='d-flex justify-content-center'>
-                <button className='d-flex justify-content-center btn btn-outline-primary' onClick={() => supporting({userObj, msgObj, isLoggedIn})}>승낙 메시지 전송 완료</button>
-              </div>
-            }
-            {msgObj.round === 3 &&
-              <div className='d-flex justify-content-center'>
-                {msgObj.text.choose == 1 && <button className='d-flex justify-content-center btn btn-outline-primary'>{msgObj.displayName} 님이 빌리는 중</button>}
-                {msgObj.text.choose == 2 && <button className='d-flex justify-content-center btn btn-outline-primary' onClick={() => confirming({userObj, msgObj})}>반납하기</button>}
-              </div>
-            }
-            {msgObj.round === 4 &&
-              <div className='d-flex justify-content-center'>
-                {msgObj.text.choose == 1 && <button className='d-flex justify-content-center btn btn-outline-primary' onClick={onClick}>반납 완료 확인</button>}
-                {msgObj.text.choose == 2 && <button className='d-flex justify-content-center btn btn-outline-primary'>주인에게 확인 중</button>}
-              </div>
-            }
-          </div>  
-        } */}
       </Link>
       <Btn msgObj={msgObj} isOwner={isOwner} userObj={userObj} isLoggedIn={isLoggedIn} num={num} points={points} setValue={setValue} counter={counter} setCounter={setCounter} />
     </div>
