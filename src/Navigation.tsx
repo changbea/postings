f6e9283b07b98ac9740b8e756d5de0b105cc373f
ChangbeomAhn <changbeomahn@ChangbeomAhnui-Macmini.local>
2024-06-08 03:24:00 +0900

    add

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -37,8 +37,9 @@ function Navigation({ isLoggedIn, userObj, setUserObj, setValue, side, setSide, 
     event.target.removeEventListener('pointermove', move)
     event.target.removeEventListener('touchmove', move)
     event.target.style.left = '-100%'
-    // if (event.pageX-offsetX < 0) {
-    // }
+    if (event.pageX-offsetX < 0) {
+      event.target.style.left = '-100%'
+    }
     checkbox(event)
   }
   const move = (event) => {
