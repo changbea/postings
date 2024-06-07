f355875bc4b697d5d2692c65738b27e7dfdd71db
ChangbeomAhn <changbeomahn@ChangbeomAhnui-Macmini.local>
2024-06-08 03:57:53 +0900

    add

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -48,6 +48,7 @@ function Navigation({ isLoggedIn, userObj, setUserObj, setValue, side, setSide, 
       document.getElementsByClassName('naving')[0].style.left=`${event.pageX-offsetSide}px`
       document.getElementsByClassName('naving')[1].style.left=`${event.pageX-offsetSide}px`
     }
+    
     // event.target.style.left = '-100%'
     // alert(event)
   }
